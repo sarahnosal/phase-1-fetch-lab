66db8bcbd9a863916bffbf5648db5b2a5486dfa5
Sarah Nosal <nosalsm@gmail.com>
2021-11-29 19:38:32 -0600

    commit tree d4c9a0b3ca75cb5979c05ebd946d7343fd4f6836

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
+  return fetch("https://anapioficeandfire.com/api/books")
+  .then(resp => resp.json())
+  .then(json => json.forEach(book => renderBooks(book)));  
 }
 
-function renderBooks(books) {
-  const main = document.querySelector('main');
-  books.forEach(book => {
-    const h2 = document.createElement('h2');
-    h2.innerHTML = book.name;
-    main.appendChild(h2);
-  });
-}
-
-document.addEventListener('DOMContentLoaded', function() {
+function renderBooks() {
   fetchBooks();
-});
+  // const main = document.querySelector('main');
+  // books.forEach(book => {
+  //   var h2 = document.createElement('h2');
+  //   h2.innerHTML = book.name;
+  //   main.appendChild(h2);
+  // });
+}
+
+// document.addEventListener('DOMContentLoaded', function() {
+//   renderBooks(fetchBooks());
+// });
+
